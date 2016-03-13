4295203c59d6ae09865b0a2c6a751784dce1f3f0
Meteoric_cry <meteoric_cry@qq.com>
2016-03-13 13:12:33 +0800

    Initial Commit

---
+++ submitAnswer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  submitAnswer
+//
+//  Created by meteoric_cry on 16/3/13.
+//  Copyright © 2016年 meteoric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ submitAnswer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  submitAnswer
+//
+//  Created by meteoric_cry on 16/3/13.
+//  Copyright © 2016年 meteoric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
